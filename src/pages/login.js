5b6477966fa7b934a01549c54dc31e8d92562dff
yamandahle <yamandahle@gmail.com>
2026-04-09 17:50:21 +0300

    added login page

---
+++ src/pages/login.js
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import "./login.css";
+import axios from "axios";
+import { useNavigate } from 'react-router-dom'
+
+
+
+const Login = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const navigate = useNavigate()
+
+
+   const handleLogin = async () => {
+    try {
+      /*save the details and send them to the server */
+      const response = await axios.post("http://127.0.0.1:8000/login", {
+        email: email,
+        password: password,
+      });
+      setMessage(response.data.message);
+      localStorage.setItem('token', response.data.token) /* save the token in local storage*/ 
+      navigate('/') /*navigate to home page if there is no error*/
+    } catch (error) {
+      setMessage("Something went wrong!");
+    }
+  }; 
+
+
+    return (
+    <div className="register-container">
+      <div className="form-box">
+        {message && <p>{message}</p>}
+        <h2> Login into your account: </h2>
+
+        <label> Email</label>
+        <input
+          type="text"
+          placeholder="Enter your email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+
+        <label> Password:</label>
+        <input
+          type="password"
+          placeholder="Enter your password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button
+          type="button"
+          className="btn btn-light"
+          onClick={handleLogin}
+        >
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Login ;
+
+
+
+
