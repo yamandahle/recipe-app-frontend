d1bf1a60fd132e108a3fea493e691b43422b730a
yamandahle <yamandahle@gmail.com>
2026-04-15 13:45:25 +0300

    added an id attribute to the recipeCard

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,35 +1,35 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import RecipeCard from '../components/recipe-card'
-
+import { useState, useEffect } from "react";
+import axios from "axios";
+import RecipeCard from "../components/recipe-card";
+import "./home.css";
 
 const Home = () => {
-    
-    const [recipes,setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState([]);
 
+  useEffect(() => {
+    /* fetch all recipes when page loads */
+    axios
+      .get("http://127.0.0.1:8000/recipes")
+      .then((res) => setRecipes(res.data.recipes));
+  }, []);
 
-    useEffect(() => {
-        /* fetch all recipes when page loads */
-        axios.get('http://127.0.0.1:8000/recipes')
-            .then(res => setRecipes(res.data.recipes))
-    }, [])
-
-
-    return (
-        <div>
-            {recipes.map(recipe => (<RecipeCard 
+  return (
+    <div className="home-container">
+      <h1 className="home-title">🍳 Recipes</h1>
+      <div className="recipes-grid">
+        {recipes.map((recipe) => (
+          <RecipeCard
             key={recipe.id}
-            title = {recipe.title}
+            id = {recipe.id}
+            title={recipe.title}
             category={recipe.category}
-            image_url ={recipe.image_url}
-            description ={recipe.description}
-             />
-
-            ))}
-        </div>
-
-
-    )
-}
+            image_url={recipe.image_url}
+            description={recipe.description}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
