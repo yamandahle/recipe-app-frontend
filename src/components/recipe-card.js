3282df1a975f71b89786909f47b0764835cb2018
yamandahle <yamandahle@gmail.com>
2026-04-15 12:06:05 +0300

    added a classNames to the title , category , description

--- src/components/recipe-card.js
+++ src/components/recipe-card.js
@@ -1,10 +1,12 @@
+import './RecipeCard.css'
+
 const RecipeCard = ({ title, category, image_url, description }) => {
     return (
-        <div>
-            <h2>{title}</h2>
-             <h4>{category}</h4>
+        <div className = "recipe-card">
+            <h2 className ="recipe-title">{title}</h2>
              <img src={image_url} alt={title} />
-             <p>{description}</p>
+             <span className ="recipe-category">{category}</span>
+             <p className ="recipe-description">{description}</p>
         </div>
     )
 }
