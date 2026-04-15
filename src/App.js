53a0ed63d84fc2a696f25f97fd9cd3153089a18f
yamandahle <yamandahle@gmail.com>
2026-04-15 15:06:43 +0300

    add all the changes if not added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/register";
 import Login from "./pages/login";
 import Home from "./pages/home";
+import RecipePage from "./pages/recipePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/recipes/:id" element={<RecipePage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
