9b9ca07bb3750c72c1b798e157cb467734f53328
yamandahle <yamandahle@gmail.com>
2026-04-15 13:42:47 +0300

    added onClick to the recipe card to make it clickable and added navigate to go to the stailed page of the recipe

--- src/components/recipe-card.js
+++ src/components/recipe-card.js
@@ -1,13 +1,18 @@
 import './RecipeCard.css'
+import { useNavigate } from 'react-router-dom';
 
-const RecipeCard = ({ title, category, image_url, description }) => {
+const RecipeCard = ({ id , title, category, image_url, description }) => {
+        const navigate = useNavigate()  
+    const goToRecipePage = () => { navigate(`/recipes/${id}`) }
     return (
-        <div className = "recipe-card">
+        
+        <div className = "recipe-card" onClick={goToRecipePage}>
             <h2 className ="recipe-title">{title}</h2>
              <img src={image_url} alt={title} />
              <span className ="recipe-category">{category}</span>
              <p className ="recipe-description">{description}</p>
         </div>
+        
     )
 }
 
