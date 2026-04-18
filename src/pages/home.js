c96b89ad6f621aea5a6c7f8067d5158930a917b5
yamandahle <yamandahle@gmail.com>
2026-04-18 14:11:27 +0300

    added a  search box in the home page with styling page

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,6 +5,7 @@ import "./home.css";
 
 const Home = () => {
   const [recipes, setRecipes] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     /* fetch all recipes when page loads */
@@ -13,14 +14,35 @@ const Home = () => {
       .then((res) => setRecipes(res.data.recipes));
   }, []);
 
+  //filter the recipes according to the search words
+  const filteredRecipes = recipes.filter(
+    (recipe) =>
+      recipe.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      recipe.category.toLowerCase().includes(searchQuery.toLowerCase()),
+  );
+
   return (
     <div className="home-container">
       <h1 className="home-title">🍳 Recipes</h1>
+
+      
+      {/*show the search box*/}
+      <div className="search-container">
+        <span className="search-icon">🔍</span>
+        <input
+          type="text"
+          placeholder="Search recipes by name or category..."
+          className="search-input"
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </div>
+
+
       <div className="recipes-grid">
-        {recipes.map((recipe) => (
+        {filteredRecipes.map((recipe) => (
           <RecipeCard
             key={recipe.id}
-            id = {recipe.id}
+            id={recipe.id}
             title={recipe.title}
             category={recipe.category}
             image_url={recipe.image_url}
